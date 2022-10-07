3ea436901b39c0d6cb57374ad126eece97d0c6db
JeevanPoojary <jeevan.poojary@nichi.com>
2022-10-07 17:18:08 +0900

    intial commit of base 1.0

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <AppSidebar />
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+      <AppHeader />
+      <div class="body flex-grow-1 px-3">
+        <CContainer>
+          <transition name="fade" mode="out-in">
+            <router-view :key="$route.path"></router-view>
+          </transition>
+        </CContainer>
+      </div>
+      <AppFooter />
+    </div>
+  </div>
+</template>
+<script>
+import { CContainer } from '@coreui/vue'
+import AppFooter from '@/components/AppFooter.vue'
+import AppHeader from '@/components/AppHeader.vue'
+import AppSidebar from '@/components/AppSidebar.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    AppFooter,
+    AppHeader,
+    AppSidebar,
+    CContainer,
+  },
+}
+</script>
---
+++ src/layouts/_nav.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    component: 'CNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    badge: {
+      color: 'primary',
+      text: 'main',
+    },
+  },
+
+  {
+    component: 'CNavGroup',
+    name: 'Base',
+    to: '/base',
+    icon: 'cil-puzzle',
+    items: [
+      {
+        component: 'CNavItem',
+        name: 'Accordion',
+        to: '/base/accordion',
+      },
+    ],
+  },
+]
